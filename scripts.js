7f125abed203cb9d8a7ca03af444be889a3e3cb2
m-mstr <128099870+m-mstr@users.noreply.github.com>
2023-03-20 18:32:51 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,3 +30,28 @@ navClose.addEventListener('click', () => {
     });
 });
 
+//  Scroll Effects //
+
+/* FUTURE USE 
+
+const faders = document.querySelectorAll('.fade');
+const appearOptions = {
+    threshold: 0
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries,appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            entry.target.classList.add("appear");
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    }) 
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+}); 
+
+*/
